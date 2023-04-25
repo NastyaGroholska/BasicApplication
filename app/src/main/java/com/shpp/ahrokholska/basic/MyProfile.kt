29ba0714c99c433151ff1237acf057fe94773ac1
Anastasiia <anastasiiahrokholska@gmail.com>
2023-04-25 16:48:22 +0300

    Initial BasicApplication project

---
+++ app/src/main/java/com/shpp/ahrokholska/basic/MyProfile.kt
@@ -0,0 +1,11 @@
+package com.shpp.ahrokholska.basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MyProfile : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_profile)
+    }
+}
