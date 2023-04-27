c4b468d31bfa4dbac5b187a4dbf48c76af7f84a2
Anastasiia <anastasiiahrokholska@gmail.com>
2023-04-27 23:25:31 +0300

    "Register" button on SignUp Activity now opens MyProfile Activity and sends it parsed user name if email and password are valid. Changed icons for buttons in MyProfile

--- app/src/main/java/com/shpp/ahrokholska/basic/MyProfile.kt
+++ app/src/main/java/com/shpp/ahrokholska/basic/MyProfile.kt
@@ -2,11 +2,15 @@ package com.shpp.ahrokholska.basic
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.shpp.ahrokholska.basic.databinding.MyProfileBinding
 
 class MyProfile : ComponentActivity() {
+    private lateinit var binding: MyProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.my_profile)
+        binding = MyProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.name.text=intent.getStringExtra(SignUp.USER_NAME)
     }
 }
--- app/src/main/java/com/shpp/ahrokholska/basic/SignUp.kt
+++ app/src/main/java/com/shpp/ahrokholska/basic/SignUp.kt
@@ -1,8 +1,10 @@
 package com.shpp.ahrokholska.basic
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import androidx.activity.ComponentActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.shpp.ahrokholska.basic.databinding.SignUpBinding
@@ -10,80 +12,91 @@ import com.shpp.ahrokholska.basic.databinding.SignUpBinding
 class SignUp : ComponentActivity() {
     private lateinit var binding: SignUpBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = SignUpBinding.inflate(layoutInflater)
-
-        binding.emailGroup.setErrorTextAppearance(R.style.errorMessage)
-        binding.passwordGroup.setErrorTextAppearance(R.style.errorMessage)
-
-        setContentView(binding.root)
-
-        setInputErrorListeners(binding.editTextTextEmailAddress, ::processEmail)
-        setInputErrorListeners(binding.editTextTextPassword, ::processPassword)
+    companion object IntentOptions {
+        const val USER_NAME = "com.shpp.ahrokholska.basic.SignUp.UserName"
     }
 
-    /**
-     * Adds listener to [viewToListenTo] when [viewToListenTo] loses focus or
-     * when the enter key is pressed invokes [processStr]
-     * @param viewToListenTo Should have TextInputLayout as a parent
-     */
-    private fun setInputErrorListeners(
-        viewToListenTo: TextInputEditText,
-        processStr: (str: String, errorReceiver: TextInputLayout) -> Unit
+    inner class Wrapper(
+        val input: TextInputEditText, val errorDisplay: TextInputLayout, val errorMessage: String,
+        private val isValid: (str: String) -> Boolean
     ) {
-        val errorReceiver = viewToListenTo.parent.parent as TextInputLayout
+        init {
+            errorDisplay.setErrorTextAppearance(R.style.errorMessage)
+            setInputErrorListeners(input, ::processInput)
+        }
 
-        viewToListenTo.setOnFocusChangeListener { view, hasFocus ->
-            if (!hasFocus) {
-                processStr((view as TextInputEditText).text.toString(), errorReceiver)
+        /**
+         * Adds listener to [viewToListenTo] when [viewToListenTo] loses focus or
+         * when the enter key is pressed invokes [processStr]
+         */
+        private fun setInputErrorListeners(
+            viewToListenTo: TextInputEditText, processStr: () -> Unit
+        ) {
+            viewToListenTo.setOnFocusChangeListener { view, hasFocus ->
+                if (!hasFocus) {
+                    processStr()
+                }
+            }
+
+            viewToListenTo.setOnEditorActionListener { view, actionId, event ->
+                processStr()
+                true
             }
         }
 
-        viewToListenTo.setOnEditorActionListener { view, actionId, event ->
-            processStr((view as TextInputEditText).text.toString(), errorReceiver)
-            true
+        /**
+         * If [input] text is valid according to [isValid] then sets error message in [errorDisplay]
+         * to null, otherwise sets it to [errorMessage]. Returns if [input] text is valid
+         */
+        fun processInput(): Boolean {
+            return if (isValid(input.text.toString())) {
+                errorDisplay.error = null
+                true
+            } else {
+                errorDisplay.error = errorMessage
+                false
+            }
         }
     }
 
-    /**
-     * If [email] is valid then sets error message in [errorReceiver] to null,
-     * otherwise sets it to @strings/incorrect_mail
-     */
-    private fun processEmail(email: String, errorReceiver: TextInputLayout) {
-        checkString(
-            email, ::isEmailValid, resources.getString(R.string.incorrect_mail),
-            errorReceiver
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SignUpBinding.inflate(layoutInflater)
+        val email = Wrapper(
+            binding.editTextTextEmailAddress, binding.emailGroup,
+            resources.getString(R.string.incorrect_mail), ::isEmailValid
         )
-    }
-
-    /**
-     * If [password] is valid then sets error message in [errorReceiver] to null,
-     * otherwise sets it to @strings/incorrect_password
-     */
-    private fun processPassword(password: String, errorReceiver: TextInputLayout) {
-        checkString(
-            password, ::isPasswordValid, resources.getString(R.string.incorrect_password),
-            errorReceiver
+        val password = Wrapper(
+            binding.editTextTextPassword, binding.passwordGroup,
+            resources.getString(R.string.incorrect_password), ::isPasswordValid
         )
+
+        setContentView(binding.root)
+
+        binding.registerButton.setOnClickListener {
+            val isEmailValid = email.processInput()
+            val isPasswordValid = password.processInput()
+
+            if (isEmailValid && isPasswordValid) {
+                startActivity(Intent(this, MyProfile::class.java).apply {
+                    putExtra(USER_NAME, email.input.text.toString()
+                        .split("@")[0].split("[+._%\\-]+".toRegex())
+                        .joinToString(separator = " ") { it ->
+                            it[0].uppercase() + it.substring(1).lowercase()
+                        })
+                })
+            } else {
+                Snackbar.make(it, R.string.signup_error, Snackbar.LENGTH_SHORT).setAnchorView(it)
+                    .show()
+            }
+        }
     }
 
-    /**
-     * If [str] is valid according to [isValid] then sets error message in [errorReceiver] to null,
-     * otherwise sets it to [error]
-     */
-    private fun checkString(
-        str: String, isValid: (str: String) -> Boolean,
-        error: String, errorReceiver: TextInputLayout
-    ) {
-        errorReceiver.error = if (isValid(str)) null else error
-    }
-
-    fun isEmailValid(email: String): Boolean {
+    private fun isEmailValid(email: String): Boolean {
         return email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
-    fun isPasswordValid(password: String): Boolean {
+    private fun isPasswordValid(password: String): Boolean {
         return password.isNotEmpty() && password.length >= 8
     }
 }
