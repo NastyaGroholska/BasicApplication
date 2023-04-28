524d75dac022c587122afec051c6e4d0e3c07c8a
Anastasiia <anastasiiahrokholska@gmail.com>
2023-04-28 20:12:54 +0300

    removed compouse

---
+++ app/src/main/java/com/shpp/ahrokholska/basicapplication/MyProfile.kt
@@ -0,0 +1,24 @@
+package com.shpp.ahrokholska.basicapplication
+
+import android.os.Bundle
+import android.transition.Slide
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.shpp.ahrokholska.basicapplication.databinding.MyProfileBinding
+
+class MyProfile : AppCompatActivity() {
+    private lateinit var binding: MyProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MyProfileBinding.inflate(layoutInflater)
+        binding.name.text = intent.getStringExtra(SignUp.USER_NAME)
+
+        with(window) {
+            requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+            enterTransition = Slide()
+        }
+
+        setContentView(binding.root)
+    }
+}
