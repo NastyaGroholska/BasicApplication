4c3815af552fc87506f073f9c712e70d4087f16f
Stanislav Dumchykov <mechanikea@gmail.com>
2023-05-13 13:09:50 +0300

    Task-2: Changes after review.

--- app/src/main/java/com/shpp/ahrokholska/basicapplication/data/ContactsDB.kt
+++ app/src/main/java/com/shpp/ahrokholska/basicapplication/data/ContactsDB.kt
@@ -18,6 +18,9 @@ class ContactsDB {
     }
 
     private fun findNewId(): Int {
+        // but not safely. What will be if delete few elements in the end of list and after that add new one?
+        // https://dou.ua/lenta/articles/interview-questions-android-developer/ item #115
+        // id:Long = UUID.randomUUID().mostSignificantBits
         var max = 0
         contacts.value.forEach {
             max = max(max, it.id)
@@ -34,6 +37,7 @@ class ContactsDB {
     }
 
     fun removeWithId(id: Int) {
+        // TODO alternative: _contacts.value = _contacts.value.toMutableList().apply { removeIf{it.id == id} }
         _contacts.update { value ->
             val mutableValue = value.toMutableList()
             mutableValue.removeIf { it.id == id }
