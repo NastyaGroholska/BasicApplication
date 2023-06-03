70dee03ad53602881fd40f432e3a2ddcb541efd1
Anastasiia <anastasiiahrokholska@gmail.com>
2023-06-03 21:27:58 +0300

    Moved json parsing to Default Dispatcher

--- app/src/main/java/com/shpp/ahrokholska/basicapplication/data/utils/Parser.kt
+++ app/src/main/java/com/shpp/ahrokholska/basicapplication/data/utils/Parser.kt
@@ -1,15 +1,17 @@
 package com.shpp.ahrokholska.basicapplication.data.utils
 
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object Parser {
-    inline fun <reified T> parseBody(body: String): T {
+    suspend inline fun <reified T> parseBody(body: String): T = withContext(Dispatchers.Default) {
         var newBody = body
         val openBrackets = body.count { it == '{' }
         val closeBrackets = body.count { it == '}' }
         if (openBrackets != closeBrackets) { //some responses don't have closing '}' at the end =/
             newBody += '}'
         }
-        return Gson().fromJson(newBody, T::class.java)
+        Gson().fromJson(newBody, T::class.java)
     }
 }
